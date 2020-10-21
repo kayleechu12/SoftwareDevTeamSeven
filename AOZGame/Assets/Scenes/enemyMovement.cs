57a1012414190b0c19dd51bc0f616b710db75636
Sutton <33501521+PBHotdog@users.noreply.github.com>
2020-10-21 12:32:46 -0600

    Update enemyMovement.cs
    small change

--- AOZGame/Assets/Scenes/enemyMovement.cs
+++ AOZGame/Assets/Scenes/enemyMovement.cs
@@ -3,15 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyMovement : MonoBehaviour
 {
     public float detectRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMashAgent>();
     }
 
     // Update is called once per frame

a9d69e6116cfc25ebb548744f3c6a78e4cc9ba0d
Sutton <33501521+PBHotdog@users.noreply.github.com>
2020-11-03 15:16:42 -0600

    Implented New Character Model with Animations
    Character can still walk through walls... but thats a fix for later

---
+++ AOZGame/Assets/Scenes/playerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Animator anim;
+    public CharacterController controller;
+
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+
+    public float rotationRate = 360;
+
+    public float moveSpeed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("vert", Input.GetAxis("Vertical"));
+        float moveAxis = Input.GetAxisRaw(moveInputAxis);
+        float turnAxis = Input.GetAxisRaw(turnInputAxis);
+        ApplyInput(moveAxis, turnAxis);
+    }
+    private void ApplyInput(float moveInput, float turnInput)
+    {
+        Move(moveInput);
+        Turn(turnInput);
+    }
+    private void Move(float input)
+    {
+        transform.Translate(Vector3.forward * input * moveSpeed);
+    }
+    private void Turn(float input)
+    {
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+    }
+}
